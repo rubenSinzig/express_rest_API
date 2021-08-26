f0c7a02b43355e6a24d5022f6147011810f5e0b9
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-26 12:38:22 +0200

    userModel -> create blueprint (Schema)

---
+++ model/userModel.js
@@ -0,0 +1,7 @@
+// require (import) __________________________________
+
+const mongoose = require("mongoose");
+
+// create Schema (blueprint) _________________________
+
+const userDataSchema = new mongoose.Schema({});
