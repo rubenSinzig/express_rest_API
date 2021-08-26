4135669d6f988b6d843d1a3093a1b32192c87d99
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-26 13:13:58 +0200

    userModel -> create mongoose.Schema()

--- model/userModel.js
+++ model/userModel.js
@@ -4,4 +4,41 @@ const mongoose = require("mongoose");
 
 // create Schema (blueprint) _________________________
 
-const userDataSchema = new mongoose.Schema({});
+const userDataSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  userPass: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  age: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  fbw: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  toolStack: {
+    type: Array,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  userAdded: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("userData", userDataSchema);
