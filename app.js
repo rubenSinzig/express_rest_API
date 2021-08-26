7b2d9da8197fc893da61f4040a595e181c812a8c
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-26 12:14:42 +0200

    app.js -> connection to mongoDB

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ const app = express();
 app.use(morgan("dev")); // console.log(app.use(morgan("dev"))) <------ take a look again. why we need this line?
 
 app.use(express.json()); // process the json data
+
+const DB_URL = process.env.DB_URL; // url for mongoDB
+
+// connection to mongoDB  ____________________________
+
+mongoose
+  .connect(DB_URL)
+  .then(console.log(`mongoDB is connected. url : ${DB_URL}`))
+  .catch((err) =>
+    console.log(`DB is NOT connected see error message : ${err.message}`)
+  );
