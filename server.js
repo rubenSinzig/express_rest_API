7e1db155218694d5490af9b4649915230d2cbef8
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-26 11:15:40 +0200

    server.js -> create server & connection

---
+++ server.js
@@ -0,0 +1,19 @@
+// ________________ SERVER CONNECTION ________________
+
+// require (import) __________________________________
+
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+
+// set port __________________________________________
+
+const PORT = process.env.PORT || 5000;
+
+// create server _____________________________________
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`); // to make sure that the Server is working
+});
